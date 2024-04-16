1f85c80a65faf02f74c38c2ee702f61c4dcc5c50
vinicius-dev <viniciusgabfarias123@gmail.com>
2024-04-16 14:44:37 -0300

    feat: about page.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page: React.FC = () => {
+    return (
+        <div className='mt-32'>
+            <h1>About Page</h1>
+            <p>Welcome to the About page!</p>
+        </div>
+    );
+};
+
+export default Page;
